8660849bc4b8fba7ed3be11a9211b82adc33b2fe
Alex <VoinovAlex1900@yandex.ru>
2024-05-20 23:44:43 +0300

    stare animate complete

--- Frontend/src/Pages/StartPage/StartPage.tsx
+++ Frontend/src/Pages/StartPage/StartPage.tsx
@@ -13,40 +13,65 @@ enum AnimateStep {
 }
 
 const delayByAppearanceMain = 1000;
-const firstStepDelay = 2000;
-const secondStepDelay = 3000;
-const thirdStepDelay = 3000;
+const firstStepDelay = 1000;
+const secondStepDelay = 2000;
+const thirdStepDelay = 2000;
 
-const additionalDelayPerAnimation = 7000;
+const additionalDelayPerAnimation = 6000;
 
 const defintStyleLine = (step: number) => {
     if (step === AnimateStep.preparation)
         return {
             opacity: 0,
-            left: '42vw',
+            left: '32vw',
         }
     if (step === AnimateStep.appearanceLine)
         return {
             opacity: 1,
-            animation: `${styles.lightbeam} 0.8s 0.1s`,
+            animation: `${styles.lightbeam} 1s 1.8s`,
         }
     if ([AnimateStep.appearanceText, AnimateStep.shiftText].includes(step))
-        return { opacity: 1 }
+        return {
+            opacity: 1,
+            animation: `${styles.lightbeam} 1s 1.8s`,
+        }
+    if (step === AnimateStep.completion) return {
+        display: 'none'
+    }
     return { opacity: 0 } as CSSProperties
 }
 
 const defineStyleText = (step: number): CSSProperties => {
+    if (step === AnimateStep.preparation) return {
+        color: 'transparent',
+        height: '0vw',
+    }
     if (step === AnimateStep.appearanceLine) return {
         color: '#fff',
-        height: 'min(5.20833vw, 9.25926vh)'
+        height: '0vw',
+        paddingTop: '5vw',
     }
-
+    if (step === AnimateStep.appearanceText) return {
+        color: '#fff',
+    }
+    if (AnimateStep.shiftText === step) return {
+        top: '1.94444vh',
+        left: '4.16667vw',
+        color: '#E7EDF3',
+        fontWeight: '500',
+        fontSize: 'min(1.66667vw, 2.96296vh)',
+        textShadow: '0 0.37037vh min(0.20833vw, 0.37037vh) rgba(0, 0, 0, 0.35)',
+        width: '12.70833vw',
+        overflow: 'visible'
+    }
+    if (step === AnimateStep.completion) return {
+        display: 'none'
+    }
     return {}
 }
 
 const StartPage = () => {
-    console.log(styles);
-    const currentWelcomePhrase = defineTimeInterval();
+    const [currentWelcomePhrase, setWelcomePhrase] = useState(defineTimeInterval());
     const [animateStep, setAnimateStep] = useState(AnimateStep.preparation);
     const completionDelay = useRef<number>(0);
     useEffect(() => {
@@ -58,6 +83,7 @@ const StartPage = () => {
                 localStorage.setItem('welcomePhrase', currentTimeInterval);
                 setTimeout(() => setAnimateStep(AnimateStep.appearanceLine), firstStepDelay);
                 setTimeout(() => setAnimateStep(AnimateStep.appearanceText), firstStepDelay + secondStepDelay);
+                setTimeout(() => setWelcomePhrase('DCC CERTIFICATE'), firstStepDelay + secondStepDelay + thirdStepDelay * 1.2);
                 setTimeout(() => setAnimateStep(AnimateStep.shiftText), firstStepDelay + secondStepDelay + thirdStepDelay);
                 completionDelay.current += additionalDelayPerAnimation;
             }
